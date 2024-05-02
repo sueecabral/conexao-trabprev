8a7ac2ee603cfb175807f6bade7dc3f4822eeca4
Suelen Cabral <sueecabral@gmail.com>
2024-05-02 20:14:39 -0300

    ajuste na logica de js para o menu hamburguer

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
-const hamburguer = document.querySelector(".hamburguer");
+// const hamburguer = document.querySelector(".hamburguer");
 
+// const nav = document.querySelector(".nav");
+
+// hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+
+
+
+const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
-
+hamburguer.addEventListener("click", () => {
+  nav.classList.toggle("active");
+  
+  if (nav.classList.contains("active")) {
+    // Desativar scroll quando o menu estiver ativo
+    document.body.style.overflow = 'hidden';
+  } else {
+    // Ativar scroll quando o menu estiver fechado
+    document.body.style.overflow = '';
+  }
+});
