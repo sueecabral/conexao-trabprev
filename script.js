b44e04e3dbaa728af0ff48671b3fe605a1172498
Suelen Cabral <sueecabral@gmail.com>
2024-05-15 17:09:45 -0300

    ajustando menu hamburguer

--- script.js
+++ script.js
@@ -6,33 +6,19 @@
 
 
 
-// const hamburguer = document.querySelector(".hamburguer");
-// const nav = document.querySelector(".nav");
-
-// hamburguer.addEventListener("click", () => {
-//   nav.classList.toggle("active");
-  
-//   if (nav.classList.contains("active")) {
-//     // Desativar scroll quando o menu estiver ativo
-//     document.body.style.overflow = 'hidden';
-//   } else {
-//     // Ativar scroll quando o menu estiver fechado
-//     document.body.style.overflow = '';
-//   }
-// });
-
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => {
   nav.classList.toggle("active");
   
-  // Verificar se o menu hamburguer está ativo
   if (nav.classList.contains("active")) {
-    // Adicionar classe para ocultar overflow
-    document.body.classList.add('menu-active');
+    // Desativar scroll quando o menu estiver ativo
+    document.body.style.overflow = 'hidden';
   } else {
-    // Remover classe para restaurar o overflow padrão
-    document.body.classList.remove('menu-active');
+    // Ativar scroll quando o menu estiver fechado
+    document.body.style.overflow = '';
   }
 });
+
+
