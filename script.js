884e0b7927e3572ff7cc43a31b701df01b66e600
Suelen Cabral <sueecabral@gmail.com>
2024-05-15 08:52:19 -0300

    ajustando o scroll do menu hamburguer

--- script.js
+++ script.js
@@ -6,17 +6,37 @@
 
 
 
+// const hamburguer = document.querySelector(".hamburguer");
+// const nav = document.querySelector(".nav");
+
+// hamburguer.addEventListener("click", () => {
+//   nav.classList.toggle("active");
+  
+//   if (nav.classList.contains("active")) {
+//     // Desativar scroll quando o menu estiver ativo
+//     document.body.style.overflow = 'hidden';
+//   } else {
+//     // Ativar scroll quando o menu estiver fechado
+//     document.body.style.overflow = '';
+//   }
+// });
+
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => {
   nav.classList.toggle("active");
-  
+
   if (nav.classList.contains("active")) {
+    // Defina a altura máxima do menu como a altura da janela
+    nav.style.maxHeight = window.innerHeight + 'px';
     // Desativar scroll quando o menu estiver ativo
     document.body.style.overflow = 'hidden';
   } else {
+    // Remova a altura máxima do menu
+    nav.style.maxHeight = null;
     // Ativar scroll quando o menu estiver fechado
     document.body.style.overflow = '';
   }
 });
+
