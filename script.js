8e57cfafb7fed82bb056d47d2cc22dff6ddaf74e
Suelen Cabral <sueecabral@gmail.com>
2024-06-28 08:11:56 -0300

    adiçao de botão ler mais no campo artigos juridicos

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const nav = document.querySelector(".nav");
 
 hamburguer.addEventListener("click", () => {
   nav.classList.toggle("active");
-  
+
   if (nav.classList.contains("active")) {
     // Desativar scroll quando o menu estiver ativo
     document.body.style.overflow = 'hidden';
@@ -13,4 +13,17 @@ hamburguer.addEventListener("click", () => {
   }
 });
 
+
+
+function toggleText() {
+  const text = document.querySelector('.about-artigos p');
+  const btn = document.querySelector('.btn-read-more');
+  if (text.classList.contains('show')) {
+    text.classList.remove('show');
+    btn.textContent = 'Ler mais';
+  } else {
+    text.classList.add('show');
+    btn.textContent = 'Ler menos';
+  }
+}
 
